702b1f28236a8df57bcc9356ed74321d65c9638b
bagor gorba <oscarsquard@gmail.com>
2025-05-08 17:08:52 +0700

    membuat crud suplier

---
+++ app/Filament/Resources/SuplierResource/Pages/CreateSuplier.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\SuplierResource\Pages;
+
+use App\Filament\Resources\SuplierResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSuplier extends CreateRecord
+{
+    protected static string $resource = SuplierResource::class;
+}
